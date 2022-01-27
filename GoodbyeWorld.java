5308f40985c894f90ee960b46685fa8372aa2912
GorErd94 <98538117+GorErd94@users.noreply.github.com>
2022-01-27 19:48:43 +0100

    Update GoodbyeWorld.java

--- GoodbyeWorld.java
+++ GoodbyeWorld.java
@@ -1,7 +1,7 @@
 /** 
- * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ * The GoodbyeWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class GoodbyeWorld {
   public static void main(String[] args) {
     System.out.println("Goodbye, World");
   }
